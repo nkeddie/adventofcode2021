a7eb8e2124d1402f99a8a5a3bdbbb2b3602c7f85
NKeddie <nate.keddie@gmail.com>
2021-12-12 11:08:06 -0500

    Day 11

--- src/Advent2021.ConsoleApp/Program.cs
+++ src/Advent2021.ConsoleApp/Program.cs
@@ -1,4 +1,4 @@
 ﻿using Advent2021.ConsoleApp;
 
-Console.WriteLine(await new Day10A().RunAsync_Simple());
-Console.WriteLine(await new Day10B().RunAsync_Simple());
+Console.WriteLine(await new Day11A().RunAsync_Simple());
+Console.WriteLine(await new Day11B().RunAsync_Simple());
